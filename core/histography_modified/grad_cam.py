30dd4225f7dd24d89bb3610eed8c973c80f04542
re0078 <reza.abdollahzadeh12@gmail.com>
2023-07-12 23:41:52 +0330

    feat: custom BaseExplainer

--- core/histography_modified/grad_cam.py
+++ core/histography_modified/grad_cam.py
@@ -5,8 +5,45 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-from histocartography.interpretability.base_explainer import BaseExplainer
 from histocartography.interpretability.grad_cam import GradCAM
+from histocartography.pipeline import PipelineStep
+
+
+from abc import abstractmethod
+from typing import Optional, Tuple
+import dgl
+import numpy as np
+import torch
+import os
+
+
+class BaseExplainer(PipelineStep):
+    """Base pipelines step"""
+
+    def __init__(
+        self,
+        model: Optional[torch.nn.Module] = None,
+        **kwargs
+    ) -> None:
+        """Abstract class that defines an explainer.
+
+        Args:
+            model_path (Optional[str], optional): Model path to pre-trained model. The path can be local or an MLflow URL. Defaults to None.
+            model (Optional[torch.nn.Module], optional): PyTorch model to use. Defaults to None.
+        """
+
+        super().__init__(**kwargs)
+
+        # look for GPU
+        self.cuda = torch.cuda.is_available()
+        self.device = torch.device("cuda:0" if self.cuda else "cpu")
+
+        # set model
+        self.model = model
+        # self.model.eval()
+        self.model = self.model.to(self.device)
+        self.model.zero_grad()
+
 
 class BaseGraphGradCAMExplainer(BaseExplainer):
     def __init__(
